f2c7bf8e9688201b0a2fd370c8a73390a10fee36
ahhaahah <43072977+ahhaahah@users.noreply.github.com>
2018-09-07 22:31:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -102,5 +102,22 @@ client.on('message', msg => {
 
 });
 
+
+
+
+client.on('message', message =>{
+    if(message.content === '#ping'){
+let start = Date.now(); message.channel.send('pong').then(message => { 
+message.edit(`\`\`\`js
+Time taken: ${Date.now() - start} ms
+Discord API: ${client.ping.toFixed(0)} ms\`\`\``);
+    });
+    }
+});
+
+
+
+
+
 client.login(process.env.BOT_TOKEN)
 
