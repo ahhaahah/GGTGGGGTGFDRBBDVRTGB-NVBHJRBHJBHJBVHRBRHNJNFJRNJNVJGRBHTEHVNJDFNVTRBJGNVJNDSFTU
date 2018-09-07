ebebd3e8880dffefdfa19bc7f5cad6196394cd6d
ahhaahah <43072977+ahhaahah@users.noreply.github.com>
2018-09-07 22:33:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -106,7 +106,7 @@ client.on('message', msg => {
 
 
 client.on('message', message =>{
-    if(message.content === '#ping'){
+    if(message.content === '!ping'){
 let start = Date.now(); message.channel.send('pong').then(message => { 
 message.edit(`\`\`\`js
 Time taken: ${Date.now() - start} ms
