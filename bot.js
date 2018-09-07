32eb586167b541574996d4d01714848b61279eba
ahhaahah <43072977+ahhaahah@users.noreply.github.com>
2018-09-07 23:46:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -105,27 +105,7 @@ client.on('message', msg => {
 
 
 
-client.on('message',async msg => {
-  var p = "!";
-  if(msg.content.startsWith(p + "user")) {
-  if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **go play minecraft**');
-  if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
-  msg.guild.createChannel(`يتم تحضير الروم :[]` , 'voice').then(time => {
-    time.overwritePermissions(msg.guild.id, {https://discord.gg/edKfbm
-      CONNECT: true,
-      SPEAK: true
-    });
-  setInterval(() => {
-      var currentTime = new Date(),
-Year = currentTime.getFullYear(),
-Month = currentTime.getMonth() + 1,
-Dat = currentTime.getDate()
-      time.setName(`Members : ◤ → ${client.users.size} ← ◢`);
- },1000);
-  });
-  }
- 
-});
+
 
 
 
