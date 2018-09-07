81edb82b7ff3991c1b6852f4990e4f65fff4a78b
ahhaahah <43072977+ahhaahah@users.noreply.github.com>
2018-09-07 23:37:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -105,21 +105,50 @@ client.on('message', msg => {
 
 
 
-client.on('message', message =>{
-    if(message.content === '!ping'){
-let start = Date.now(); message.channel.send('pong').then(message => { 
-message.edit(`\`\`\`js
-Time taken: ${Date.now() - start} ms
-Discord API: ${client.ping.toFixed(0)} ms\`\`\``);
-    });
+client.on("message", async message => {
+if(message.channel.type === "dm") return;
+ if(message.content === (prefix + "B-Time")) { /// حط اي كلمة تبيها
+ if (!message.channel.guild) return message.reply('**هذا الامر للسيرفرات فقط**');
+    let uptime = client.uptime;
+
+    let days = 0;
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
+    let notCompleted = true;
+
+    while (notCompleted) {
+
+        if (uptime >= 8.64e+7) { ///لا تعدل اي شي 
+
+            days++;
+            uptime -= 8.64e+7;
+
+        } else if (uptime >= 3.6e+6) {
+
+            hours++;
+            uptime -= 3.6e+6;
+
+        } else if (uptime >= 60000) {
+
+            minutes++;
+            uptime -= 60000;
+
+        } else if (uptime >= 1000) {
+            seconds++;
+            uptime -= 1000;
+
+        }
+
+        if (uptime < 1000)  notCompleted = false;
+
     }
-});
-client.on('message', msg => {fuck you}
-  if (msg.content === 'discord.gg') {
-   msg.delete(30)
-    msg.reply('ممنوع');
-  }
-});
+
+    message.channel.send("`" + `${days} days, ${hours} hrs, ${minutes} min , ${seconds} sec` + "`");
+
+
+}
+ });
 
 
 
